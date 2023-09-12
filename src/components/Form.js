fba9779a25d60a84c25e5723a1fd80e27ccc1b1b
Eduardo Tornquist <eatornquist04@gmail.com>
2023-09-12 13:55:26 -0400

    using state to control elements

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,20 +1,46 @@
+import { useState } from 'react'
+
 export default function Form() {
+  const [description, setDescription] = useState('')
+  const [quantity, setQuantity] = useState(1)
+
   function handleSubmit(e) {
-    e.preventDefault() //Thuis is added to prevent the reload when the form is submitted
-    console.log(e)
+    e.preventDefault() //This is added to prevent the reload when the form is submitted
+
+    if (!description) return
+
+    const newItem = { description, quantity, packed: false, id: Date.now() }
+    console.log(newItem)
+
+    setDescription('')
+    setQuantity(1)
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => {
+          // console.log(e.target.value)
+          setQuantity(Number(e.target.value))
+        }}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => {
+          // console.log(e.target)
+          setDescription(e.target.value)
+        }}
+      />
       <button>Add</button>
     </form>
   )
