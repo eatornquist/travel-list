fc7c00cd91a3e099658733814e8ed0f753ebe4b0
Eduardo <eduardo.tornquist@lennar.com>
2023-11-13 11:38:30 -0500

    Working in the delete item functionality. Child to parent communication.

--- src/App.js
+++ src/App.js
@@ -3,19 +3,24 @@ import Form from './components/Form'
 import PackingList from './components/PackingList'
 import Stats from './components/Stats'
 import { useState } from 'react'
+import initialItems from './data'
 
 export default function App() {
-  const [items, setItems] = useState([])
+  const [items, setItems] = useState(initialItems)
 
   function handleAddItems(item) {
     setItems((items) => [...items, item])
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id))
   }
 
   return (
     <div className="app">
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <PackingList items={items} />
+      <PackingList items={items} onDeleteItem={handleDeleteItem} />
       <Stats />
     </div>
   )
--- src/components/PackingList.js
+++ src/components/PackingList.js
@@ -1,12 +1,12 @@
 // import initialItems from '../data'
 import Item from './Item'
 
-export default function PackingList({ items }) {
+export default function PackingList({ items, onDeleteItem }) {
   return (
     <div className="list">
       <ul className="list">
         {items.map((item) => (
-          <Item item={item} key={item.id} />
+          <Item item={item} onDeleteItem={onDeleteItem} key={item.id} />
         ))}
       </ul>
     </div>
